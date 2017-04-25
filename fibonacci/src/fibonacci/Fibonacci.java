0a15fb151db3e553d914ba45d80972ad4fa6380f
Mike Ryan <mr@mry4n.net>
2017-04-25 19:47:24 -0400

    using BigInteger and storing Fib Nums as they are calculated

--- fibonacci/src/fibonacci/Fibonacci.java
+++ fibonacci/src/fibonacci/Fibonacci.java
@@ -1,47 +1,59 @@
 package fibonacci;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Fibonacci {
-	
-	static Integer getFibonacciNumber(int n){
-		
+
+	// Use a map so you don't recalculate every Fibonacci number more than once
+	static Map<Integer,BigInteger> fibMap = new HashMap<>();
+
+	static BigInteger getFibonacciNumber(int n){
+
 		// 0	1	2	3	4	5	
 		// 0	1	1	2	3	5	8	13	21	34	55	89	144	233	377	610	987	1597	2584	4181	6765
-		
-		if (n==0){
-			return 0;
-		} else if (n==1){
-			return 1;
-		}else{
-			return getFibonacciNumber(n-1) + getFibonacciNumber(n-2);
+
+		if (fibMap.containsKey(n)){
+			return fibMap.get(n);
+		} else {
+			if (n==0){
+				fibMap.put(0, new BigInteger("0"));
+				return fibMap.get(0);
+			} else if (n==1){
+				fibMap.put(1, new BigInteger("1"));
+				return fibMap.get(1);
+			}else{
+				BigInteger fibNumber = getFibonacciNumber(n-2).add(getFibonacciNumber(n-1));
+				fibMap.put(n, fibNumber);
+				return fibMap.get(n);
+			}
 		}
 	}
-	
-	static List<Integer> getFibonacciList(int n){
-		
-		List<Integer> fibList = new ArrayList<>();
+
+	static List<BigInteger> getFibonacciList(int n){
+
+		List<BigInteger> fibList = new ArrayList<>();
 		for(int i=0; i<=n; i++){
 			fibList.add(getFibonacciNumber(i));
 		}
 		return fibList;
 	}
-	
+
 	static String printFibList(int n) {
 		int counter = 0;
 		String fibListString = "";
-		List<Integer> fibList = getFibonacciList(n);
-		for(Integer fibNum: fibList){
+		List<BigInteger> fibList = getFibonacciList(n);
+		for(BigInteger fibNum: fibList){
 			fibListString = fibListString + "[" + counter + "]" + " => " + fibNum + "\n";
 			counter++;
 		}
 		return fibListString;
 	}
-	
-	
+
+
 	static void printFibListOnTheFly(int n){
 		for(int i=0; i<=n; i++){
 			System.out.println("["+i+"] " + getFibonacciNumber(i));
