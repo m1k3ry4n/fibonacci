0c06e8577477864d69f9ec07d98abc97f61bbbc9
Mike Ryan <mr@mry4n.net>
2017-04-26 07:19:10 -0400

    Hold the numbers in a list; add a method to only populate that list without returning a number that does not use recursion, but stores values in the list and retrieves them as needed; method to print numbers horizontally

--- fibonacci/src/fibonacci/Fibonacci.java
+++ fibonacci/src/fibonacci/Fibonacci.java
@@ -9,31 +9,52 @@ import java.util.Map;
 public class Fibonacci {
 
 	// Use a map so you don't recalculate every Fibonacci number more than once
-	static Map<Integer,BigInteger> fibMap = new HashMap<>();
+	private List<BigInteger> fibArrayList;
 
-	static BigInteger getFibonacciNumber(int n){
+	public Fibonacci(int n){
+		fibArrayList = new ArrayList<>(n);
+	}
+
+	public BigInteger getFibonacciNumber(int n){
 
 		// 0	1	2	3	4	5	
 		// 0	1	1	2	3	5	8	13	21	34	55	89	144	233	377	610	987	1597	2584	4181	6765
 
-		if (fibMap.containsKey(n)){
-			return fibMap.get(n);
-		} else {
+		if (fibArrayList.get(n) == null){
 			if (n==0){
-				fibMap.put(0, new BigInteger("0"));
-				return fibMap.get(0);
+				fibArrayList.add(0, new BigInteger("0"));
+				return fibArrayList.get(0);
 			} else if (n==1){
-				fibMap.put(1, new BigInteger("1"));
-				return fibMap.get(1);
+				fibArrayList.add(1, new BigInteger("1"));
+				return fibArrayList.get(1);
 			}else{
 				BigInteger fibNumber = getFibonacciNumber(n-2).add(getFibonacciNumber(n-1));
-				fibMap.put(n, fibNumber);
-				return fibMap.get(n);
+				fibArrayList.add(n, fibNumber);
+				return fibArrayList.get(n);
 			}
+		} else {
+			return fibArrayList.get(n);
 		}
 	}
 
-	static List<BigInteger> getFibonacciList(int n){
+	public void populateFibArrayList(int n){
+
+		int index = 0;
+
+		while (index <= n){
+			if (index==0){
+				fibArrayList.add(0, new BigInteger("0"));
+			} else if (index==1){
+				fibArrayList.add(1, new BigInteger("1"));
+			}else{
+				BigInteger fibNumber = fibArrayList.get(index-2).add(fibArrayList.get(index-1));
+				fibArrayList.add(index, fibNumber);
+			}
+			index++;
+		}
+	}
+
+	public List<BigInteger> getFibonacciList(int n){
 
 		List<BigInteger> fibList = new ArrayList<>();
 		for(int i=0; i<=n; i++){
@@ -42,22 +63,31 @@ public class Fibonacci {
 		return fibList;
 	}
 
-	static String printFibList(int n) {
+	public String printFibArrayList() {
 		int counter = 0;
 		String fibListString = "";
-		List<BigInteger> fibList = getFibonacciList(n);
-		for(BigInteger fibNum: fibList){
+		for(BigInteger fibNum: fibArrayList){
 			fibListString = fibListString + "[" + counter + "]" + " => " + fibNum + "\n";
 			counter++;
 		}
 		return fibListString;
 	}
+	
+	public String printFibArrayListHorizontally(){
+		int counter = 0;
+		String fibListIndexes = "";
+		String fibListNumbers = "";
+		for(BigInteger fibNum: fibArrayList){
+			fibListIndexes = fibListIndexes + "[" + counter + "]";
+			fibListNumbers = fibListNumbers + fibNum + " ";
+			counter++;
+		}
+		return fibListIndexes + "\n" + fibListNumbers;
+	}
 
-
-	static void printFibListOnTheFly(int n){
+	public void printFibListOnTheFly(int n){
 		for(int i=0; i<=n; i++){
 			System.out.println("["+i+"] " + getFibonacciNumber(i));
 		}
 	}
-
 }
--- fibonacci/src/fibonacci/TestRunner.java
+++ fibonacci/src/fibonacci/TestRunner.java
@@ -5,7 +5,11 @@ public class TestRunner {
 	
 	public static void main(String arg[]){
 		int fibNum=100;
+		Fibonacci fib = new Fibonacci(fibNum);
 		System.out.println("The Fibonacci Numbers up to " + fibNum + " are: ");
-		Fibonacci.printFibListOnTheFly(fibNum);
+		fib.populateFibArrayList(fibNum);
+		System.out.println(fib.printFibArrayList());
+		System.out.println(fib.printFibArrayListHorizontally());
 	}
+	
 }
